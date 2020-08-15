4737c71839a4192ef8564ee48b7a84d8e9c40050
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2020-08-15 15:42:37 +0100

    project_front
    wait

---
+++ full-version/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="dashboard-dashboard">
+
+        <p class="mb-4"><a href="https://github.com/SortableJS/Vue.Draggable" target="_blank" rel="nofollow">Vue.Draggable</a> - Vue component allowing drag-and-drop and synchronization with view model array. Based on and offering all features of Sortable.js</p>
+
+        
+        <drag-and-drop-multiple-lists></drag-and-drop-multiple-lists>
+        
+    </div>
+</template>
+
+<script>
+import DragAndDropMultipleLists from "./components/extra-components/drag-and-drop/DragAndDropMultipleLists.vue"
+
+
+export default {
+    components: {
+       
+        DragAndDropMultipleLists,
+       
+    }
+}
+</script>
