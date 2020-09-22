06e386dc9bb859da53f5fac595afa36409ca79e1
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2020-09-22 21:10:48 +0100

    test

--- full-version/src/views/apps/user/user-list/cell-renderer/CellRendererActions.vue
+++ full-version/src/views/apps/user/user-list/cell-renderer/CellRendererActions.vue
@@ -35,7 +35,10 @@
 
             /* UnComment below lines for enabling true flow if deleting user */
             this.$http.delete("http://localhost:8087/users/deleteUser/"+ this.params.data.iduser)
-               .then(()   => { this.showDeleteSuccess() 
+               .then(()   => { this.showDeleteSuccess() ;
+                 this.$http.get('http://localhost:8087/users/allUsers' ,{headers : {'Authorization' :"Bearer "  + localStorage.accessToken}}).then((result) => {
+         this.usersData=result.data;          
+       });
                })
                .catch(err => { console.error(err)       })
           },
