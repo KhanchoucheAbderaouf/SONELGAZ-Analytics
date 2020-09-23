27f367915cb2b586fed8a065067db8ebd7d3a3b4
KhanchoucheAbderaouf <a.khanchouche@esi-sba.dz>
2020-09-23 15:34:06 +0100

    f

--- full-version/src/views/apps/user/user-edit/UserEdit.vue
+++ full-version/src/views/apps/user/user-edit/UserEdit.vue
@@ -22,7 +22,7 @@
       <div slot="no-body" class="tabs-container px-6 pt-6">
 
         <vs-tabs v-model="activeTab" class="tab-action-btn-fill-conatiner">
-          <vs-tab label="Account" icon-pack="feather" icon="icon-user">
+          <vs-tab label="Compte utilisateur" icon-pack="feather" icon="icon-user">
             <div class="tab-text">
               <user-edit-tab-account class="mt-4" :data="user_data" />
             </div>
--- full-version/src/views/apps/user/user-edit/UserEditTabAccount.vue
+++ full-version/src/views/apps/user/user-edit/UserEditTabAccount.vue
@@ -10,25 +10,7 @@
 <template>
   <div id="user-edit-tab-info">
 
-    <!-- Avatar Row -->
-    <div class="vx-row">
-      <div class="vx-col w-full">
-        <div class="flex items-start flex-col sm:flex-row">
-          <img :src="data.avatar" class="mr-8 rounded h-24 w-24" />
-          <!-- <vs-avatar :src="data.avatar" size="80px" class="mr-4" /> -->
-          <div>
-            <p class="text-lg font-medium mb-2 mt-4 sm:mt-0">{{ data.nom }}</p>
-            <input type="file" class="hidden" ref="update_avatar_input" @change="update_avatar" accept="image/*">
-
-            <!-- Toggle comment of below buttons as one for actual flow & currently shown is only for demo -->
-            <vs-button class="mr-4 mb-4">Change Avatar</vs-button>
-            <!-- <vs-button type="border" class="mr-4" @click="$refs.update_avatar_input.click()">Change Avatar</vs-button> -->
-
-            <vs-button type="border" color="danger">Remove Avatar</vs-button>
-          </div>
-        </div>
-      </div>
-    </div>
+    
 
     <!-- Content Row -->
     <div class="vx-row">
