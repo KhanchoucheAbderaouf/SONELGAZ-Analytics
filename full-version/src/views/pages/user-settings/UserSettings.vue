c693326bdb8146927324ad3f8cd60063050e4a0c
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2020-09-25 13:03:12 +0100

    user setting

--- full-version/src/views/pages/user-settings/UserSettings.vue
+++ full-version/src/views/pages/user-settings/UserSettings.vue
@@ -2,17 +2,17 @@
   <vs-tabs :position="isSmallerScreen ? 'top' : 'left'" class="tabs-shadow-none" id="profile-tabs" :key="isSmallerScreen">
 
     <!-- GENERAL -->
-    <vs-tab icon-pack="feather" icon="icon-user" :label="!isSmallerScreen ? 'General' : ''">
+    <!--vs-tab icon-pack="feather" icon="icon-user" :label="!isSmallerScreen ? 'General' : ''">
       <div class="tab-general md:ml-4 md:mt-0 mt-4 ml-0">
         <user-settings-general />
       </div>
-    </vs-tab>
-    <vs-tab icon-pack="feather" icon="icon-lock" :label="!isSmallerScreen ? 'Change Password' : ''">
+    </vs-tab-->
+    <vs-tab icon-pack="feather" icon="icon-lock" :label="!isSmallerScreen ? 'Change Username Password' : ''">
       <div class="tab-change-pwd md:ml-4 md:mt-0 mt-4 ml-0">
         <user-settings-change-password />
       </div>
     </vs-tab>
-    <vs-tab icon-pack="feather" icon="icon-info" :label="!isSmallerScreen ? 'Info' : ''">
+    <!--vs-tab icon-pack="feather" icon="icon-info" :label="!isSmallerScreen ? 'Info' : ''">
       <div class="tab-info md:ml-4 md:mt-0 mt-4 ml-0">
         <user-settings-info />
       </div>
@@ -31,26 +31,17 @@
       <div class="tab-text md:ml-4 md:mt-0 mt-4 ml-0">
         <user-settings-notifications />
       </div>
-    </vs-tab>
+    </vs-tab-->
   </vs-tabs>
 </template>
 
 <script>
-import UserSettingsGeneral from "./UserSettingsGeneral.vue"
 import UserSettingsChangePassword from "./UserSettingsChangePassword.vue"
-import UserSettingsInfo from "./UserSettingsInfo.vue"
-import UserSettingsSocialLinks from "./UserSettingsSocialLinks.vue"
-import UserSettingsConnections from "./UserSettingsConnections.vue"
-import UserSettingsNotifications from "./UserSettingsNotifications.vue"
 
 export default {
   components: {
-    UserSettingsGeneral,
     UserSettingsChangePassword,
-    UserSettingsInfo,
-    UserSettingsSocialLinks,
-    UserSettingsConnections,
-    UserSettingsNotifications,
+   
   },
   data() {
     return {
--- full-version/src/views/pages/user-settings/UserSettingsChangePassword.vue
+++ full-version/src/views/pages/user-settings/UserSettingsChangePassword.vue
@@ -1,14 +1,20 @@
 <template>
   <vx-card no-shadow>
 
-    <vs-input class="w-full mb-base" label-placeholder="Old Password" v-model="old_password" />
-    <vs-input class="w-full mb-base" label-placeholder="New Password" v-model="new_password" />
-    <vs-input class="w-full mb-base" label-placeholder="Confirm Password" v-model="confirm_new_password" />
+    <vs-input class="w-full mb-base" label-placeholder="username" v-validate="'required'" v-model="username" />
+    <vs-input class="w-full mb-base"  ref="password"  type="password" data-vv-validate-on="blur" v-validate="'required'" name="password"
+     label-placeholder="New Password"  v-model="new_password" />
+    <vs-input class="w-full mb-base" type="password"
+      v-validate="'confirmed:password'"
+      data-vv-validate-on="blur"
+      data-vv-as="password"
+      name="confirm_password"
+      label-placeholder="Confirm Password"  v-model="confirm_new_password" />
 
     <!-- Save & Reset Button -->
     <div class="flex flex-wrap items-center justify-end">
-      <vs-button class="ml-auto mt-2">Save Changes</vs-button>
-      <vs-button class="ml-4 mt-2" type="border" color="warning">Reset</vs-button>
+      <vs-button class="ml-auto mt-2" @click="submitForm">Save Changes</vs-button>
+     
     </div>
   </vx-card>
 </template>
@@ -17,9 +23,36 @@
 export default {
   data() {
     return {
-      old_password: "",
+      username: "",
       new_password: "",
       confirm_new_password: "",
+    }
+  },
+    methods: {
+    submitForm() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.$http.put('http://localhost:8087/users/updateUserNamePass/'+this.$store.state.AppActiveUser.uid,{'username':this.username,'password':this.confirm_new_password},
+     {headers : {'Authorization' :"Bearer "  + localStorage.accessToken}})
+        .then(this.$vs.notify({
+              color: 'success',
+              title: 'User Updated',
+              text: 'The selected user was successfully Updated'
+            }),
+           this.$router.push("/dashboard/ecommerce") 
+            ).catch(error => {
+        this.$vs.loading.close();
+         this.$vs.notify({
+        title: ' Requet erroné  ',
+        text: error,
+        color: 'danger'
+      })
+      });
+         
+        } else {
+          // form have errors
+        }
+      })
     }
   },
   computed: {
